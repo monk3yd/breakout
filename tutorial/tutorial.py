50f3b8eae687aa8f10ab725b128f3f9ede9e2088
monk3yd <jegajardog@gmail.com>
2022-03-04 19:16:14 -0300

    First commit

---
+++ tutorial/tutorial.py
@@ -0,0 +1,31 @@
+import pygame
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("PyGame Tutorial")
+
+WHITE = (255, 255, 255)
+
+FPS = 60
+
+def draw_window():
+    WIN.fill(WHITE)
+    pygame.display.update()
+
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw_window()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()

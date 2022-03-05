db687652b2d0c6f5df4d03effb0fc700d29e74c9
monk3yd <jegajardog@gmail.com>
2022-03-05 17:59:34 -0300

    Bar, Border, Ball PNG and Basic Bounce. Starting Block Pending.

--- main.py
+++ main.py
@@ -9,24 +9,49 @@ pygame.display.set_caption("Breakout Game")
 # RGB Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 # Frames Per Second
 FPS = 60
 
 # Border
-BORDER = pygame.Rect(0, 0,  WIDTH, 20)
+TOP_BORDER = pygame.Rect(0, 0,  WIDTH, 10)
+LEFT_BORDER = pygame.Rect(0, 0,  10, HEIGHT-60)
+RIGHT_BORDER = pygame.Rect(490, 0,  10, HEIGHT-60)
 
 # Bar Dimensions
 BAR_WIDTH, BAR_HEIGHT = (150, 20)
-
-# BAR SPEED
+# Bar Speed
 VEL = 3
 
+# Ball
+BALL_WIDTH, BALL_HEIGHT = (23, 23)
+BALL_IMAGE = pygame.image.load(os.path.join("img", "ball.png"))
+BALL = pygame.transform.scale(BALL_IMAGE, (BALL_WIDTH, BALL_HEIGHT))
+# Ball Speed
+BALL_VEL = [2, 2]  # x, y
 
-def draw_window(bar):
+
+def draw_window(bar, ball, blocks):
+    # Background
     WIN.fill(BLACK)
-    pygame.draw.rect(WIN, WHITE, BORDER)
+    # Draw Border
+    pygame.draw.rect(WIN, WHITE, TOP_BORDER)
+    pygame.draw.rect(WIN, WHITE, LEFT_BORDER)
+    pygame.draw.rect(WIN, WHITE, RIGHT_BORDER)
+
+    # Draw Bar
     pygame.draw.rect(WIN, WHITE, bar)
+
+    # Draw Ball
+    WIN.blit(BALL, (ball.x, ball.y))
+    # pygame.draw.rect(WIN, WHITE, ball)
+    # pygame.draw.circle(WIN, WHITE, [WIDTH//2, HEIGHT//2], 5, 0)  # Surface, color, pos, radius, width
+
+    # Draw Blocks
+    for block in blocks:
+        pygame.draw.rect(WIN, RED, block)
+    # Update Frame
     pygame.display.update()
 
 
@@ -38,9 +63,35 @@ def handle_bar_movement(keys, bar):
         bar.x += VEL
 
 
+def handle_ball_movement(ball, bar):
+    ball.x += BALL_VEL[0]
+    ball.y += BALL_VEL[1]
+
+    # BOUNCE
+    if LEFT_BORDER.colliderect(ball) or RIGHT_BORDER.colliderect(ball):
+        BALL_VEL[0] *= -1
+
+    if TOP_BORDER.colliderect(ball):
+        BALL_VEL[1] *= -1
+
+    if bar.colliderect(ball):
+        BALL_VEL[1] *= -1
+
+
+def handle_blocks(blocks):
+    for block in blocks:
+        pass
+
+
 def main():
-    bar = pygame.Rect(WIDTH/2 - 75, 650, BAR_WIDTH, BAR_HEIGHT)
-    # TODO - ball
+    # Bar Rectangle
+    bar = pygame.Rect(WIDTH/2 - 75, HEIGHT - 50, BAR_WIDTH, BAR_HEIGHT)  # x, y, width, height
+    # Ball Rectangle
+    ball = pygame.Rect(WIDTH//2, HEIGHT//2, BALL_WIDTH, BALL_HEIGHT)
+    # Blocks
+    blocks = []
+    block = pygame.Rect(20, 80, 50, 20)
+    blocks.append(block)
 
     clock = pygame.time.Clock()
     run = True
@@ -53,7 +104,9 @@ def main():
 
         keys_pressed = pygame.key.get_pressed()
         handle_bar_movement(keys_pressed, bar)
-        draw_window(bar)
+        handle_ball_movement(ball, bar)
+        handle_blocks(blocks)
+        draw_window(bar, ball, blocks)
 
 
 if __name__ == "__main__":
